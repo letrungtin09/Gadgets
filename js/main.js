079b0cbeb87a0e28a53bf221ba4d199a40c0e256
letrungtin09 <letrungtin1312@gmail.com>
2023-09-23 00:51:05 +0700

    fill information products and fix css for website

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ $(document).ready(function () {
     swipeToSlide: true,
   });
 });
+
+$(document).ready(function () {
+  $(".bestseller__slider2").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrow: true,
+    swipeToSlide: true,
+  });
+});
+
+$(document).ready(function () {
+  $(".bestseller__slider3").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrow: true,
+    swipeToSlide: true,
+  });
+});
