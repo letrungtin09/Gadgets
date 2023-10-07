4acfe4f3586acf235cb4083326988fa8dfb067f1
letrungtin09 <letrungtin1312@gmail.com>
2023-10-07 10:22:41 +0700

    code page detail product

--- js/main.js
+++ js/main.js
@@ -27,3 +27,29 @@ $(document).ready(function () {
     swipeToSlide: true,
   });
 });
+
+$(document).ready(function () {
+  $(".productIntro__slide").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".productIntro__nav",
+  });
+  $(".productIntro__nav").slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: ".productIntro__slide",
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true,
+    verticalSwiping: true,
+  });
+});
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Thumbs: {
+    type: "classic",
+  },
+});
